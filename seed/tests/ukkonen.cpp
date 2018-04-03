274dafd1ea2db951ac9a4ef87625ea1911c6c8c4
Patryk Czajka <patryk.czajka@student.uw.edu.pl>
2018-04-03 20:10:58 +0200

    One more test kond for ukkonen algorithm

--- seed/tests/ukkonen.cpp
+++ seed/tests/ukkonen.cpp
@@ -7,9 +7,12 @@ string w;
 vector<int> word;
 
 int sum;
+vector<int>dp;
 void dfs_count(Node& v) {
     for(auto &it: v.edges) {
         Edge& e = it.second;
+        dp[v.depth + 1]++;
+        dp[st.node(e.node).depth + 1]--;
         sum += e.len();
         dfs_count(st.node(e.node));
     }
@@ -35,6 +38,7 @@ void dfs_print(Node& v, int id) {
     }
 }
 
+
 int main() {
     cin >> w;
     int N;
@@ -43,11 +47,16 @@ int main() {
         word.push_back(c - 97);
     word.push_back(-1);
     st.create(word);
-    
-//     st.print();
-    
+
+    dp.resize(word.size() + 10);
     dfs_count(st.root());
     printf("%d\n", sum);
+    printf("0");
+    for(int i = 1; i <= N; ++i) {
+        dp[i] += dp[i - 1];
+        printf(" %d", dp[i]);
+    }
+    puts("");
     
     printf("%d", st.sa[0]);
     for(int i = 1; i < N; ++i) {
--- seed/tests/ukkonen_slow.py
+++ seed/tests/ukkonen_slow.py
@@ -5,10 +5,13 @@ w += '$'
 N = len(w)
 sa = []
 
+wlens = [set() for i in xrange(N + 1)]
+
 words = set()
 for i in range(N):
     for j in range(i + 1, N + 1):
         words.add(w[i:j])
+        wlens[j - i].add(w[i:j])
     sa.append((w[i:], i))
 
 def lcp_(a, b):
@@ -17,13 +20,17 @@ def lcp_(a, b):
     return i - 1
 
 sa.sort()
+# number if words
 print len(words)
+# number of words of some len
+print ' '.join(str(len(x)) for x in wlens)
+# suffix array
 print ' '.join(str(x) for _,x in sa)
-
+# sorted suffixes
 print '\n'.join(str(x) for x,_ in sa)
-
 lcp = []
 for i in range(N - 1):
     lcp.append(lcp_(sa[i][0], sa[i + 1][0]))
 
+# lcp aray
 print ' '.join(str(x) for x in lcp + [0])
